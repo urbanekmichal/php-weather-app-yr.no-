bcab3a1e69a5f37cfb5b1d10801ba50337d003e9
Michal Urbánek <michal.urbanek@zajezdy.cz>
2020-02-13 14:17:36 +0100

    php for downloading xml

---
+++ downloader.php
@@ -0,0 +1,48 @@
+<?php
+require("connect.php");
+
+function recoverLastUpdate($connection){
+    $sql_recover = "UPDATE `pocasi_lokality` SET `last_update`='2020-01-01 00:00:00' WHERE 1";
+    $result = mysqli_query($connection, $sql_recover);
+    if ($result) {
+        echo "Last updates was updated";
+    }
+}
+
+function saveHourData($url, $filename, $connection)
+{
+    $dom = new DOMDocument();
+    $dom->load($url);
+    $dom->save("./download-hour/$filename");
+    $now = date("Y-m-d H:i:s");
+    $sql_update_change = "UPDATE pocasi_lokality SET last_update = '" . $now . "' where filename = '" . $filename . "'";
+    $result = mysqli_query($connection, $sql_update_change);
+    if ($result) {
+        echo $filename." downloaded";
+    }
+}
+
+function checkHourData($connection)
+{
+    $yest = new DateTime();
+    $yest->add(DateInterval::createFromDateString('yesterday'));
+    $yest_sql = date("Y-m-d H:i:s", $yest->getTimestamp());
+    $sql_select = "SELECT url_parameter, filename FROM pocasi_lokality where last_update < '$yest_sql'";
+    $result = mysqli_query($connection, $sql_select);
+    if ($result) {
+        while ($row = mysqli_fetch_assoc($result)):
+            $filename = $row['filename'];
+            $urlLocality = $row['url_parameter'];
+            $url = "https://www.yr.no/place/Czech_Republic/URL_PARAMETER/forecast_hour_by_hour.xml";
+            $url = str_replace("URL_PARAMETER", $urlLocality, $url);
+            saveHourData($url, $filename, $connection);
+            break;
+        endwhile;
+    }
+}
+
+
+checkHourData($connection);
+//recoverLastUpdate($connection);
+
+?>
--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php
 require("connect.php");
+//require("downloader.php");
 ?>
 <!DOCTYPE html>
 <html lang="cs">
@@ -14,40 +15,16 @@ require("connect.php");
 //$getAllLocalities = "";
 //$saveWeatherdata = "";
 
-function saveNewData($url, $filename, $connection){
-    $dom = new DOMDocument();
-    $dom -> load($url);
-    $dom -> save($filename);
-    $now = date("Y-m-d H:i:s");
-    $sql_update_change = "UPDATE pocasi_lokality SET last_update = '".$now."' where filename = '".$filename."'";
-    $result = mysqli_query($connection, $sql_update_change);
-//    if($result) ...
-}
-
-
-
-set_time_limit(60);
-$sql = "select * from pocasi_lokality order by url_parameter ASC";
+$sql = "select name_cz, filename from pocasi_lokality order by url_parameter ASC";
 $res = mysqli_query($connection, $sql);
 
 if (mysqli_num_rows($res) > 0) {
     while ($row = mysqli_fetch_assoc($res)):
         $filename = $row['filename'];
-        $urlLocality = $row['url_parameter'];
         $localityName = $row['name_cz'];
-        $lastUpdate = $row['last_update'];
-
-        $url = "https://www.yr.no/place/Czech_Republic/URL_PARAMETER/forecast_hour_by_hour.xml";
-        $url = str_replace("URL_PARAMETER", $urlLocality, $url);
 
         if(strlen($localityName)>0) {
             echo "<a href='weatherdata.php?xml=$filename'>$localityName</a><br>";
-//            $actual = new DateTime();
-//            date_sub($actual, date_interval_create_from_date_string('1 day'));
-//            COMPARING DATE WHEN DATA ARE OLDER THAN 1 DAY, CALLING saveNewData();
-//            if(){
-//                saveNewData($url, $filename, $spojeni);
-//            }
         }
     endwhile;
 }
